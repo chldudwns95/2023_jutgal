59fac5bb4d85cdddef22edfb50c78b2dc2d5235f
chldudwns95 <chldudwns95@naver.com>
2023-04-16 20:10:26 +0900

    MYSQL로 수정

---
+++ src/main/java/com/ggjg/ggbanchan/model/AutoNo.java
@@ -0,0 +1,80 @@
+package com.ggjg.ggbanchan.model;
+
+import java.util.Date;
+
+public class AutoNo {
+    private String AUTO_NO;
+    private String AUTO_NM;
+    private String REF_CODE;
+    private String REF_NAME;
+    private String COMM_CD;
+    private String PRE_FIX;
+    private String AFTER_FIX;
+    private Date REG_DATE;
+
+    public String getAUTO_NO() {
+        return AUTO_NO;
+    }
+
+    public void setAUTO_NO(String AUTO_NO) {
+        this.AUTO_NO = AUTO_NO;
+    }
+
+    public String getAUTO_NM() {
+        return AUTO_NM;
+    }
+
+    public void setAUTO_NM(String AUTO_NM) {
+        this.AUTO_NM = AUTO_NM;
+    }
+
+    public String getREF_CODE() {
+        return REF_CODE;
+    }
+
+    public void setREF_CODE(String REF_CODE) {
+        this.REF_CODE = REF_CODE;
+    }
+
+    public String getREF_NAME() {
+        return REF_NAME;
+    }
+
+    public void setREF_NAME(String REF_NAME) {
+        this.REF_NAME = REF_NAME;
+    }
+
+    public String getCOMM_CD() {
+        return COMM_CD;
+    }
+
+    public void setCOMM_CD(String COMM_CD) {
+        this.COMM_CD = COMM_CD;
+    }
+
+    public String getPRE_FIX() {
+        return PRE_FIX;
+    }
+
+    public void setPRE_FIX(String PRE_FIX) {
+        this.PRE_FIX = PRE_FIX;
+    }
+
+    public String getAFTER_FIX() {
+        return AFTER_FIX;
+    }
+
+    public void setAFTER_FIX(String AFTER_FIX) {
+        this.AFTER_FIX = AFTER_FIX;
+    }
+
+    public Date getREG_DATE() {
+        return REG_DATE;
+    }
+
+    public void setREG_DATE(Date REG_DATE) {
+        this.REG_DATE = REG_DATE;
+    }
+}
+
+
